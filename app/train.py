ac33920f06b1abb1db5f7e9a3c7c872d2d84d8fb
paulpascal <paul.alogno@gmail.com>
2024-07-16 23:45:36 +0000

    ref: update code

--- app/train.py
+++ app/train.py
@@ -10,15 +10,28 @@ iris = load_iris()
 X = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = pd.Series(iris.target)
 
+# Change column names
+X.rename(
+    columns={
+        "sepal length (cm)": "sepal_length",
+        "sepal width (cm)": "sepal_width",
+        "petal length (cm)": "petal_length",
+        "petal width (cm)": "petal_width",
+    },
+    inplace=True,
+)
+
 # Train/test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Train model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
 # Ensure the model directory exists
-os.makedirs('model', exist_ok=True)
+os.makedirs("model", exist_ok=True)
 
 # Save model
-joblib.dump(model, 'model/model.pkl')
+joblib.dump(model, "model/model.pkl")
